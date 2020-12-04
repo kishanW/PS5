8d0c66ca5a123d60eff4acc231b09c4c756c6229
Kishan Wanigasinghe <radheekakishan@gmail.com>
2020-12-03 19:32:06 -0500

    initial commit

---
+++ PS5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PS5
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			//var webDriverPathChrome = Path.Combine(Environment.CurrentDirectory, @"web-driver\");
+			//IWebDriver driver = new ChromeDriver(webDriverPathChrome);
+		}
+	}
+}
